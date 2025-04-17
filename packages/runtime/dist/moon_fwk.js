af4b209c1b7b8fdb90aae512f6be0933878339cd
igolubevJ <igolubevapp@gmail.com>
2025-04-18 00:02:12 +0300

    vdom example

--- packages/runtime/dist/moon_fwk.js
+++ packages/runtime/dist/moon_fwk.js
@@ -1,1 +1,33 @@
-console.log('This will soon be a frontend framework!');
+function withoutNulls(arr) {
+  return arr.filter((item) => item != null)
+}
+
+const DOM_TYPES = {
+  TEXT: 'text',
+  ELEMENT: 'element'};
+function h(tag, props = {}, children = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT,
+  }
+}
+function hString(str) {
+  return {
+    value: str,
+    type: DOM_TYPES.TEXT,
+  }
+}
+function mapTextNodes(children) {
+  return children.map((child) =>
+    typeof child === 'string' ? hString(child) : child
+  )
+}
+
+const vdom = h('form', { class: 'login-form', action: 'login' }, [
+  h('input', { type: 'text', name: 'user' }),
+  h('input', { type: 'password', name: 'pass' }),
+  h('button', { on: { click: () => {} } }, ['Login'])
+]);
+console.log(vdom);
--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -1,1 +1,9 @@
-console.log('This will soon be a frontend framework!')
+import { h } from './h'
+
+const vdom = h('form', { class: 'login-form', action: 'login' }, [
+  h('input', { type: 'text', name: 'user' }),
+  h('input', { type: 'password', name: 'pass' }),
+  h('button', { on: { click: () => {} } }, ['Login'])
+])
+
+console.log(vdom)
