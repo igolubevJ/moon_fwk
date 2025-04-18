04075746703cc936aa0f75e4474379579d99a932
igolubevJ <igolubevapp@gmail.com>
2025-04-18 11:11:10 +0300

    remove dom

---
+++ packages/runtime/src/destroy-dom.js
@@ -0,0 +1,50 @@
+import { DOM_TYPES } from './h'
+
+
+/// Чтобы уничтожить DOM, связанный с виртуальным узлом,
+/// нужно учесть, какому типу узла он соответствует: 
+/// - Текстовый узел - удаляется из родительского элемента
+///                    при помощи метода remove();
+/// - Узел фрагмента - каждый дочерний узел удаляется из 
+///                    родительского элемента (на который
+///                    указывает свойство el виртуального узла)
+/// - Узел элемента - нужно выполнить две предшествующие операции
+///                   и удалить обработчик событий из элемента.
+export function destroyDOM(vdom) {
+  const { type } = vdom
+
+  switch (type) {
+    case DOM_TYPES.TEXT: {
+      removeTextNode(vdom)
+      break
+    }
+
+    case DOM_TYPES.ELEMENT: {
+      removeElementNode(vdom)
+      break
+    }
+
+    case DOM_TYPES.FRAGMENT: {
+      removeFragmentNodes(vdom)
+      break
+    }
+
+    default: {
+      throw new Error(`Can't destroy DOM of type: ${type}`)
+    }
+  }
+
+  delete vdom.el
+}
+
+function removeTextNode() {
+  throw new Error('[removeTextNode] not implemented')
+}
+
+function removeElementNode() {
+  throw new Error('[removeElementNode] not implemented')
+}
+
+function removeFragmentNodes() {
+  throw new Error('[removeFragmentNodes] not implemented')
+}
