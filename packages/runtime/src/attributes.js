35d88671adf14afb9da837ad067ab1dfc3c83b00
igolubevJ <igolubevapp@gmail.com>
2025-04-18 10:17:23 +0300

    set attribute

--- packages/runtime/src/attributes.js
+++ packages/runtime/src/attributes.js
@@ -1,3 +1,29 @@
-export function setAttributes() {
-  throw new Error('[setAttributes] not implemented')
+export function setAttributes(el, attrs) {
+  const { class: className, style, ...otherAttrs } = attrs
+
+  if (className) {
+    setClass(el, className)                                 // Задает аттрибут class 
+  }
+
+  if (style) {
+    Object.entries(style).forEach(([prop, value]) => {
+      setStyle(el, prop, value)                             // Задает аттрибут style
+    })
+  }
+
+  for (const [name, value] of Object.entries(otherAttrs)) {
+    setAttribute(el, name, value)                           // Задает остальные аттрибуты
+  }
+}
+
+function setClass(el, className) {
+  throw new Error('[setClass] not implemented')
+}
+
+function setStyle(el, prop, value) {
+  throw new Error('[setStyle] not implemented')
+}
+
+function setAttribute(el, name, value) {
+  throw new Error('[setAttribute] not implemented')
 }
