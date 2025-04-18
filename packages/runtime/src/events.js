80b5e28fffc4bcd539be0cf48e680aa342a41525
igolubevJ <igolubevapp@gmail.com>
2025-04-18 09:51:21 +0300

    Add event listeners to events

--- packages/runtime/src/events.js
+++ packages/runtime/src/events.js
@@ -1,3 +1,20 @@
-export function addEventListeners() {
-  throw new Error('[addEventListeners] not implemented')
+
+/// Возвращает зарегистрированную функцию необходимую для 
+/// реализации метода destoryDOM - обработчики событий нужно
+/// будет удалять для предотвращения утечек памяти.
+export function addEventListener(eventName, handler, el) {
+  el.addEventListener(eventName, handler)
+  return handler
+}
+
+/// Позволяет добавлять несколько обработчиков событий
+export function addEventListeners(listeners = {}, el) {
+  const addedListeners = {}
+
+  Object.entries(listeners).forEach(([eventName, handler]) => {
+    const listener = addEventListener(eventName, handler, el)
+    addedListeners[eventName] = listener
+  })
+
+  return addedListeners
 }
