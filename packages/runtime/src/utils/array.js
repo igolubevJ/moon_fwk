bd41b4bab22882c7885dcefc0e93276b21fb3aad
igolubevJ <igolubevapp@gmail.com>
2025-04-20 21:38:40 +0300

    Find difference in two array

--- packages/runtime/src/utils/array.js
+++ packages/runtime/src/utils/array.js
@@ -4,3 +4,15 @@ export function withoutNulls(arr) {
   // Используем != для удаление null и undefined
   return arr.filter((item) => item != null)
 }
+
+
+export function arraysDiff(oldArray, newArray) {
+  return {
+    added: newArray.filter(
+      (newItem) => !oldArray.includes(newItem)  // Элементы нового массива, которых нет в старом массиве, добавлены
+    ),
+    removed: oldArray.filter(
+      (oldItem) => !newArray.includes(oldItem)  // Элементы старого массива, которых нет в новом, удалены
+    )
+  }
+}
